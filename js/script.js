2aaa497aa00d51079ba013d774eb2ffb7b8da06b
Ibtisumraian <ibtisumraian@gmail.com>
2025-03-01 16:58:53 +0600

    completed task alert js added

--- js/script.js
+++ js/script.js
@@ -30,9 +30,27 @@ for(let btn of completedBtn){
             ddiv.innerHTML = 
             ` <p class=" text-sm px-6 py-4 bg-blue-50 mx-6 rounded-xl mb-4">You have Complete The Task ${head2} at ${currentTime}</p>`
             activityLog.appendChild(ddiv)
+            event.target.classList.add("disabled")
             event.target.disabled = true;
+
+            // completed task alert section
+           let dd = document.querySelectorAll(".dd");
+           if(
+            dd[0].classList.contains("disabled") && 
+            dd[1].classList.contains("disabled") &&
+            dd[2].classList.contains("disabled") &&
+            dd[4].classList.contains("disabled") &&
+            dd[5].classList.contains("disabled") 
             
+        ){
+            alert("Congrates!!! You have completed all the current task")
+            
+           }
+          
+
+               
     })
+    
 }
 
 
